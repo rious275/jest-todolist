2b5e6455b85c4728e40d0ea3f311b64f382db3c6
rious275 <rious275@gmail.com>
2023-10-31 23:19:19 +0900

    테스트 코드 recoil 렌더 컴포넌트 작성

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,14 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Input from './Input';
+import { RecoilRoot } from 'recoil';
 
 describe('<Input />', () => {
+  const renderComponent = () =>
+    render(
+      <RecoilRoot>
+        <Input />
+      </RecoilRoot>
+    );
+
   it('placeholder가 제대로 노출 된다.', () => {
-    render(<Input />);
+    renderComponent();
     screen.getByPlaceholderText('할 일을 입력해주세요');
   });
 
-  it('사용자가 입력한 값으로 value가 잘 변경된다.', () => {
-    render(<Input />);
+  it('입력창에 사용자가 입력한 값으로 value가 잘 변경된다.', () => {
+    renderComponent();
     const input = screen.getByRole('todo-input') as HTMLInputElement;
     fireEvent.change(input, { target: { value: 'test' } });
     expect(input.value).toEqual('test');
